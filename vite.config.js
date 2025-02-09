02acc3c9f0731cc25242282cd18a3ecbcce505d7
boboJ821 <hx821722@gmail.com>
2025-02-10 00:33:22 +0800

    修改

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.JPG', '**/*.jpg'],
-  server: {
-    port: 3000
-  },
   resolve: {
     alias: {
-      '@': '/src',
+      '@': path.resolve(__dirname, './src'),
     },
   },
+  assetsInclude: ['**/*.jpg', '**/*.png', '**/*.gif', '**/*.svg'],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          three: ['three'],
+        }
+      }
+    }
+  }
 }) 
